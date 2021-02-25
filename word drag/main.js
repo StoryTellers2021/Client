9d8a8c20780aa8cbabc867c436c8b5f39432290c
Neem <spc1201@westminstercollege.edu>
2021-02-24 19:10:16 -0700

    word functionality error fixed

--- word drag/main.js
+++ word drag/main.js
@@ -1,15 +1,8 @@
 // This is to get the number of boxes to make.
-var word = getQuery();
+const word = getQuery();
 var num = word.length;
 var tochange = "";
 var changed = "";
-
-function getQuery() {
-    var query = window.location.search.substring(1);
-    console.log(query);
-    
-    return query;
-}
 
 // Add a specific number of boxes in the html file.
 for(var i = 0; i<num; i++) {
@@ -36,27 +29,65 @@ for(const l of letters) {
     l.addEventListener('dragend', dragEnd);
 }
 
+/*setTimeout(function() {
+    document.body.innerHTML += "<button>hint</button>";
+}, 10000);*/
+
+// function to check if the word is correct or not.
+function check() {
+    var temp = ""
+    for(const l of letters) {
+        temp += l.innerHTML;
+    }
+
+    if (temp === "hello") {
+        console.log("Correct");
+    }
+    else {
+        console.log("Close!");
+    }
+}
+
+// This is to get what the query gets so that we can make boxes accordingly.
+function getQuery() {
+    var query = window.location.search.substring(1);
+    console.log(query);
+    
+    return query;
+}
+
+// function that activates when mouse clicks.
 function dragStart() {
     tochange = this.innerHTML;
 }
 
+// function that activates when mouse is let go.
 function dragEnd() {
-    this.innerHTML = changed;
+    if("" != changed) {
+        this.innerHTML = changed;
+    }
+    tochange = "";
+    changed = "";
 }
 
+// function that activates when it enters anothers box.
 function dragEnter(e) {
     e.preventDefault();
     this.parentNode.className += ' hovered';
 }
 
+// we don't need this one.
 function dragOver(e) {
     e.preventDefault();
 }
 
+// function that activates when we leave a ceratin box.
 function dragLeave() {
     this.parentNode.className = 'box';
 }
 
+// function that activates when we drop the drag so this 
+// happens before dragEnd happens but both from mouseclick up.
 function dragDrop() {
     this.parentNode.className = 'box';
     changed = this.innerHTML;
