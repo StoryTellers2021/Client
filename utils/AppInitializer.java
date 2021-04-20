5f53801efe51ff144673ec5b28af811c5b60d7e2
BlueJ42 <56273924+BlueJ42@users.noreply.github.com>
2021-04-20 14:38:28 -0600

    Uploaded CSS and HTML

---
+++ utils/AppInitializer.java
@@ -0,0 +1,20 @@
+package utils;
+
+import javax.servlet.ServletContext;  
+import javax.servlet.ServletException;  
+import javax.servlet.ServletRegistration.Dynamic;  
+import org.springframework.web.WebApplicationInitializer;  
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
+import org.springframework.web.servlet.DispatcherServlet;  
+public class AppInitializer implements WebApplicationInitializer {
+	public void onStartup(ServletContext servletContext) throws ServletException {  
+        final AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(SpringConfig.class);
+        ctx.setServletContext(servletContext);
+        final Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        dynamic.addMapping("/api/v1/*");
+        dynamic.setLoadOnStartup(1);
+        ctx.refresh();
+        DatabaseStaticHandler.init(ctx.getBean(DatabaseQueries.class));
+   }  
+}
